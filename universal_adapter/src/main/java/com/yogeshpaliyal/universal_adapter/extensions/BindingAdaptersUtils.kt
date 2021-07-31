5aa04f7203abc737fbcb6dbd77bbee0ffec0a195
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2021-07-31 20:09:29 +0530

    added binding adapter to set recyclerview

---
+++ universal_adapter/src/main/java/com/yogeshpaliyal/universal_adapter/extensions/BindingAdaptersUtils.kt
@@ -0,0 +1,41 @@
+package com.yogeshpaliyal.universal_adapter.extensions
+
+import androidx.annotation.LayoutRes
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.yogeshpaliyal.universal_adapter.adapter.UniversalAdapterViewType
+import com.yogeshpaliyal.universal_adapter.adapter.UniversalRecyclerAdapter
+import com.yogeshpaliyal.universal_adapter.utils.Resource
+import java.util.*
+
+
+@BindingAdapter("recycler_adapter")
+fun RecyclerView.setRecyclerAdapter(adapter: RecyclerView.Adapter<*>) {
+    this.adapter = adapter
+}
+
+
+@BindingAdapter(value = ["lifecycleOwner", "data", "item_layout", "loading_layout", "error_layout", "load_more_layout", "no_data_layout", "item_listener", "error_listener", "no_data_listener"])
+fun <T> RecyclerView.setRecyclerAdapter(
+    lifecycleOwner: LifecycleOwner?,
+    data: Resource<List<T>>?,
+    @LayoutRes itemLayout: Int,
+    @LayoutRes loadingLayout: Int?,
+    @LayoutRes errorLayout: Int?,
+    @LayoutRes loadMoreLayout: Int?,
+    @LayoutRes noDataLayout: Int?,
+    itemListener: Any?,
+    errorListener: Any?,
+    noDataListener: Any?
+) {
+    val tempAdapter = UniversalRecyclerAdapter.Builder(
+        lifecycleOwner = lifecycleOwner, data = data,
+        content = UniversalAdapterViewType.Content(itemLayout, itemListener),
+        loading = UniversalAdapterViewType.Loading(loadingLayout),
+        loadingFooter = UniversalAdapterViewType.LoadingFooter(loadMoreLayout),
+        error = UniversalAdapterViewType.Error(errorLayout, errorListener),
+        noData = UniversalAdapterViewType.NoData(noDataLayout,noDataListener)
+    ).build()
+    adapter = tempAdapter.getAdapter()
+}
